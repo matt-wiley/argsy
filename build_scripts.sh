60df564e53cc4323dc0b80397a7f992e5fce92d7
Matt Wiley <mattwiley+gitlab@fastmail.com>
2021-10-08 01:03:09 -0400

    Bv test.

--- build_scripts.sh
+++ build_scripts.sh
@@ -11,8 +11,17 @@ function main() {
         "test")
             pytest
             ;;
+        "next_version")
+            version_text=$(bump2version --current-version "$(git describe --tags $(git rev-list --tags --max-count=1))" "${2}" --allow-dirty --dry-run --list)
+            echo $(echo "${version_text}" | sed 's/new_version=//')
+            ;;
         "build") 
-            python setup.py bdist_wheel 
+            echo "${1}" > version.txt
+            python setup.py bdist_wheel
+            ;;
+        "tag")
+            echo "0.1.0" > version.txt
+            bump2version --current-version "0.1.0" --new-version "${2}" --tag --allow-dirty --verbose minor 
             ;;
         "publish")
             twine upload dist/* -u "${2}" -p "${3}"
