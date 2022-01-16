224eb06b7ee56044052ddeb633229fa3b68aaf46
Matt Wiley <mattwiley+gitlab@fastmail.com>
2022-01-16 00:24:12 -0500

    Fixes the build versioning process.

--- build_scripts.sh
+++ build_scripts.sh
@@ -7,10 +7,10 @@ function main() {
             ;;
         "clean") 
             rm -rf build dist *.egg-info
-            ;;
-        "test")
             find ./ -regex ".*/__pycache__" | grep -vE "^\./\.venv" | xargs rm -rf
             find ./ -regex ".*/\.pytest_cache" | grep -vE "\./\.venv" | xargs rm -rf
+            ;;
+        "test")
             pytest -rA
             ;;
         "build") 
@@ -18,8 +18,15 @@ function main() {
             # The use of "minor" (or patch or major) is necessary in order for 
             # the version bump to occur, but since we are providing both the 
             # current and new versions the version_string overrides this argument.
+
+            # !! NOTE: This will alter the setup.py file to have the new version string
             bump2version --current-version "0.0.0" --new-version "${version_string}" --allow-dirty minor setup.py 
+
+            # Run the Python build
             python setup.py bdist_wheel
+
+            # !! NOTE: This resets the setup.py version to 0.0.0 for the next build
+            bump2version --current-version "${version_string}" --new-version "0.0.0" --allow-dirty minor setup.py 
             ;;
         "publish")
             twine upload dist/* -u "${2}" -p "${3}"
