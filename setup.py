74b3e13a53c8276108385560a3ad8aeaadcf77a0
Matt Wiley <mattwiley+gitlab@fastmail.com>
2022-01-18 10:26:17 -0500

    Update descriptions for setuptools.

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
     name=name,
     author='Matt Wiley',
     version="0.0.0",
+    description='Tiny wrapper for Python\'s `argparse` package with YAML-based cli configuration.',
+    long_description=open('README.md').read(),
     py_modules=[name],
     install_requires=open('requirements/runtime.txt','r').read().split('\n'),
 )
