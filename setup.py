77350011f0c4e918439f401c4c4bf758e225df95
Matt Wiley <mattwiley+gitlab@fastmail.com>
2022-01-18 10:31:48 -0500

    Specifying Markdown description content type.

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     version="0.0.0",
     description='Tiny wrapper for Python\'s `argparse` package with YAML-based cli configuration.',
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     py_modules=[name],
     install_requires=open('requirements/runtime.txt','r').read().split('\n'),
 )
